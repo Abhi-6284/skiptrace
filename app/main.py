7df4b11c7ae9c03f0c7fe0463d50f132e809e66e
Prashant Abhishek Lugun <108335288+Abhi-6284@users.noreply.github.com>
2025-03-21 22:47:33 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,7 +31,3 @@ async def scrape(request: ScrapeRequest):
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Google Scraper API"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)

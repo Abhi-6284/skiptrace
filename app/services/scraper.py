fcae6a3d636a02f46175bc97d3690d8572075c6b
Prashant Abhishek Lugun <108335288+Abhi-6284@users.noreply.github.com>
2025-03-22 00:02:17 +0530

    Update scraper.py

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
 import random
@@ -35,11 +36,15 @@ def scrape_google_details(querysearch):
 
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
-
+    
+    # service = Service(executable_path='chromedriver')
     driver = webdriver.Chrome(options=options)
 
     try:
